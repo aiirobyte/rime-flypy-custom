58810704e6986019e3a3d139d9469c281eaa21f8
aiirobyte <aiiroanlee@gmail.com>
2023-04-26 22:49:34 +0800

    new look and melt_eng

--- rime.lua
+++ rime.lua
@@ -1,17 +1,23 @@
-function date_translator(input, seg)
-   if (input == "orq") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " "))
-   end
+function time_translator(input, seg)
+    if (input == "time") then
+       yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "时间"))
+    end
 end
 
-function time_translator(input, seg)
-   if (input == "ouj") then
-      local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M"), " ")
-      cand.quality = 1
-      yield(cand)
-   end
+function date_translator(input, seg)
+    if (input == "date") then
+       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
+       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "日期"))
+    end
+end
+
+function week_translator(input, seg)
+    if (input == "week") then
+        arr = {"日", "二", "三", "四", "五", "六"}
+        yield(Candidate("date", seg.start, seg._end, os.date(
+                            "%Y-%m-%d 星期" .. arr[tonumber(os.date("%w"))]),
+                        ""))
+    end
 end
 
 calculator_translator = require("calculator_translator")
