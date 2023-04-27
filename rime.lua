7c8d348d093a02bfb899f6950d15d7591af05b3a
aiirobyte <aiiroanlee@gmail.com>
2023-04-27 17:27:37 +0800

    cleanup

--- rime.lua
+++ rime.lua
@@ -1,21 +1,71 @@
-function time_translator(input, seg)
-    if (input == "time") then
-       yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "时间"))
+function date_translator(input, seg, env)
+    local config = env.engine.schema.config
+    local date = config:get_string(env.name_space .. "/date") or "rq"
+    local time = config:get_string(env.name_space .. "/time") or "sj"
+    local week = config:get_string(env.name_space .. "/week") or "xq"
+    local datetime = config:get_string(env.name_space .. "/datetime") or "dt"
+    local timestamp = config:get_string(env.name_space .. "/timestamp") or "ts"
+    -- 日期
+    if (input == date) then
+        local cand = Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("date", seg.start, seg._end,
+            os.date("%Y 年 ") .. tostring(tonumber(os.date("%m"))) .. os.date(" 月 %d 日"), "")
+        cand.quality = 100
+        yield(cand)
     end
-end
-
-function date_translator(input, seg)
-    if (input == "date") then
-       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
-       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "日期"))
+    -- 时间
+    if (input == time) then
+        local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M"), "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "")
+        cand.quality = 100
+        yield(cand)
     end
-end
-
-function week_translator(input, seg)
-    if (input == "week") then
-        arr = {"日", "二", "三", "四", "五", "六"}
-        yield(Candidate("date", seg.start, seg._end, os.date(
-                            "%Y-%m-%d 星期" .. arr[tonumber(os.date("%w"))]),
-                        ""))
+    -- 星期
+    if (input == week) then
+        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+        local cand = Candidate("week", seg.start, seg._end, "星期" .. weakTab[tonumber(os.date("%w") + 1)], "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("week", seg.start, seg._end, "礼拜" .. weakTab[tonumber(os.date("%w") + 1)], "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("week", seg.start, seg._end, "周" .. weakTab[tonumber(os.date("%w") + 1)], "")
+        cand.quality = 100
+        yield(cand)
     end
+    -- ISO 8601/RFC 3339 的时间格式 （固定东八区）（示例 2022-01-07T20:42:51+08:00）
+    if (input == datetime) then
+        local cand = Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%dT%H:%M:%S+08:00"), "")
+        cand.quality = 100
+        yield(cand)
+        local cand = Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), "")
+        cand.quality = 100
+        yield(cand)
+    end
+    -- 时间戳（十位数，到秒，示例 1650861664）
+    if (input == timestamp) then
+        local cand = Candidate("datetime", seg.start, seg._end, os.time(), "")
+        cand.quality = 100
+        yield(cand)
+    end
+    -- -- 输出内存
+    -- local cand = Candidate("date", seg.start, seg._end, ("%.f"):format(collectgarbage('count')), "")
+    -- cand.quality = 100
+    -- yield(cand)
+    -- if input == "xxx" then
+    --     collectgarbage()
+    --     local cand = Candidate("date", seg.start, seg._end, "collectgarbage()", "")
+    --     cand.quality = 100
+    --     yield(cand)
+    -- end
 end
